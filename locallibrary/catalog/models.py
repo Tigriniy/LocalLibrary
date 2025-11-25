324b93cbbeac8ac3ad9e73d3b8d171f45fe325c3
Тимофей Куриной <gr434_kutal@lab-203-5.tomtit-tomsk.ru>
2025-11-25 10:03:47 +0700

    Добавление моделей и тестовых данных в админке

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 from django.urls import reverse
+import uuid
+
+class BookInstance(models.Model):
+    """Модель представляет конкретную копию книги"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Уникальный ID для этой книги')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Доступность книги'
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
 
 
 class Genre(models.Model):
--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/catalog/')),
+    path('', views.index, name='index'),
 ]
